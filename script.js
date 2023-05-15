3b7a96f51337a33050bce91ef83eed64dd979ada
Christina2208 <christinalf22@gmail.com>
2023-05-15 16:31:15 -0400

    removed past,present&future classes

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ $(function () {
 var todayDate = dayjs("2023-05-15").format("dddd, MMMM D, YYYY")
 $("#currentDay").text(todayDate);
 
+var now = dayjs()
+
+
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
